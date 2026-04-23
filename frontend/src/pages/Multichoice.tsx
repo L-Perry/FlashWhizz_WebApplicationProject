c60b698369d7de81d167cd43ea67cb83a03043df
L-Perry-Laptop <lperry@trinity.edu>
2026-04-23 16:30:34 -0500

    Added Popups for Points and Finish

--- frontend/src/pages/Multichoice.tsx
+++ frontend/src/pages/Multichoice.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Link } from "react-router-dom";
 
 
 export default function Multichoice(): React.ReactElement {
@@ -21,17 +22,22 @@ export default function Multichoice(): React.ReactElement {
     }
 
     const [points, setPoints] = useState(0);
-
+    const [answer, setAnswer] = useState(false)
+    const [showAnswer, setShowAnswer] = useState(false);
+    const [showFinish, setShowFinish] = useState(true);
     function handleAnswer(selected: string, correct: string) {
         if (selected === correct) {
             console.log("Correct!");
             setPoints(points + 1);
+            setAnswer(true);
         } else {
             console.log("Wrong!");
-            if (points>0){
-                setPoints(points - 1);
+            if (points > 0) {
+                setPoints(points + 1);
+                setAnswer(false);
             }
         }
+        setShowAnswer(true);
     }
 
     return (
@@ -50,6 +56,26 @@ export default function Multichoice(): React.ReactElement {
                     wrongAnswers={['Green', 'Orange', 'Pink']}
                 />
             </div>
+            {showAnswer && (
+                <div className="fixed inset-0 flex items-center justify-center bg-black/50 z-50">
+                    <div className="jersey-25-regular text-2xl bg-white rounded-lg p-6 shadow-xl w-full max-w-md relative">
+                        <button className="text-3xl absolute top-2 right-3" onClick={() => setShowAnswer(false)}>X</button>
+                        <h3>You got it {answer ? "right" : "wrong"}!</h3>
+                        <h3>{answer ? "Plus" : "Minus"} one point</h3>
+                    </div>
+                </div>
+            )}
+            {showFinish && (
+                <div className="fixed inset-0 flex items-center justify-center bg-black/50 z-50">
+                    <div className="jersey-25-regular text-3xl bg-white rounded-lg p-6 shadow-xl w-full max-w-md relative">
+                        <h3>You've finished the quiz with a score of {points}</h3>
+                        <div className="flex justify-around mt-3">
+                            <Link to="/multichoice/1" className="jersey-25-regular bg-[#7E52A0]/50 rounded-[10px] border-2 border-black box-border p-3 hover:bg-[#7E52A0]/75">Play Again?</Link>
+                            <Link to="/" className="jersey-25-regular bg-[#7E52A0]/50 rounded-[10px] border-2 border-black box-border p-3 hover:bg-[#7E52A0]/75">Go Home</Link>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
 
 
