17535574d7948cbe2f871aeebdfe541b5c3d8e7e
L-Perry-Laptop <lperry@trinity.edu>
2026-04-26 17:25:55 -0500

    Added some linking through study method

--- frontend/src/pages/StudyMethod.tsx
+++ frontend/src/pages/StudyMethod.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import "./StudyMethod.css"
+import { useParams, Link } from 'react-router-dom';
 
 
-function GameOption({ name, position }: { name: string, position: string }) {
+function GameOption({ name, position, link }: { name: string, position: string, link: string }) {
     return (
-        <button className={`OptionBtn ${position}`}>{name}</button>
+        <Link to={link || "#"} className={`OptionBtn ${position} ${!link ? "disabled" : ""}`}>{name}</Link>
     )
 }
 
 export default function StudyMethod(): React.ReactElement {
+    const { id } = useParams()
+
     return (
         <div className="jersey-25-regular">
             <div className="StudyMethodTitle">
@@ -16,11 +19,11 @@ export default function StudyMethod(): React.ReactElement {
                 <h1>Quiz Name</h1>
             </div>
             <div className="OptionsContainer">
-                <GameOption name="Matching" position="" />
-                <GameOption name="Flashcards" position="top-10 left-150" />
-                <GameOption name="Quiz" position="top-90 left-60" />
-                <GameOption name="Asteroids" position="top-190 left-25" />
-                <GameOption name="Crossword" position="top-150 left-150" />
+                <GameOption link="" name="Matching" position="" />
+                <GameOption link="" name="Flashcards" position="top-10 left-150" />
+                <GameOption link={`/multichoice/${id}`} name="Quiz" position="top-90 left-60" />
+                <GameOption link="" name="Asteroids" position="top-190 left-25" />
+                <GameOption link="" name="Crossword" position="top-150 left-150" />
             </div>
 
         </div>
